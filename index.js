3603c7a1699f1b22e72d695e43bd1bff55469447
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2024-11-03 09:46:04 +0600

    add user env

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
-
+// middleWare
+app.use(cors())
+app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3umb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
