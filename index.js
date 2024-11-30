a2a6692aab6548b7278e4794fc8f981937b4dbfd
abdullah-al-muhammad <abdullahalmuhammad6@gmail.com>
2024-11-30 15:45:24 +0600

    add user data to fetch user specific data

--- index.js
+++ index.js
@@ -14,76 +14,91 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
-    const tourismSpotCollection = client.db('spotDB').collection('addSpot')
-    const tourismSpotUserCollection = client.db('spotDB').collection('user')
+		const tourismSpotCollection = client.db('spotDB').collection('addSpot')
+		const tourismSpotUserCollection = client.db('spotDB').collection('user')
 
-    app.post('/addSpot', async(req, res) =>{
-      const addSpot = req.body
-      console.log('add tourist spot', addSpot);
-      const result = await tourismSpotCollection.insertOne(addSpot)
-      res.send(result)
-    })
+		app.post('/addSpot', async (req, res) => {
+			const addSpot = req.body
+			console.log('add tourist spot', addSpot);
+			const result = await tourismSpotCollection.insertOne(addSpot)
+			res.send(result)
+		})
 
-    app.get('/addSpot', async(req, res) =>{
-      const cursor = tourismSpotCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+		app.get('/addSpot', async (req, res) => {
+			const cursor = tourismSpotCollection.find()
+			const result = await cursor.toArray()
+			res.send(result)
+		})
 
-    app.get('/addSpot/:id', async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await tourismSpotCollection.findOne(query)
-      res.send(result)
-    })
-
-
-    // user Information
-    app.post("/user", async(req, res) =>{
-      const users = req.body
-      const result = await tourismSpotUserCollection.insertOne(users)
-      console.log(result);
-      res.send(result)
-    })
-
-    app.get('/user', async (req, res) => {
-      const result = await tourismSpotUserCollection.find().toArray()
-      res.send(result)
-    })
-    app.get('/user/:userId', async (req, res) => {
-      const userId = req.params.userId
-      const query ={_id: new ObjectId(userId)} 
-      const result = await tourismSpotUserCollection.findOne(query)
-      res.send(result)
-    })
+		app.get('/addSpot/:id', async (req, res) => {
+			const id = req.params.id
+			const query = { _id: new ObjectId(id) }
+			const result = await tourismSpotCollection.findOne(query)
+			res.send(result)
+		})
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+		// user Information
+		app.post("/user", async (req, res) => {
+			const users = req.body
+			const result = await tourismSpotUserCollection.insertOne(users)
+			console.log(result);
+			res.send(result)
+		})
+
+		app.get('/user', async (req, res) => {
+			const email = req.query.e
+			if (!email) {
+				return res.status(400).send({ error: 'Email is required' });
+			}
+			try {
+				const result = await tourismSpotUserCollection.findOne( {email} )
+				if (result) {
+					res.send(result)
+				} else {
+					res.status(404).send({ error: 'User not found' })
+				}
+
+			} catch (err) {
+				console.error('Error fetching user:', err);
+				res.status(500).send({ error: 'Internal Server Error' });
+			}
+		})
+
+		app.get('/user/:userId', async (req, res) => {
+			const userId = req.params.userId
+			const query = { _id: new ObjectId(userId) }
+			const result = await tourismSpotUserCollection.findOne(query)
+			res.send(result)
+		})
+
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) =>{
-    res.send('port is running')
+app.get('/', (req, res) => {
+	res.send('port is running')
 })
-app.listen(port, ()=>{
-    console.log(`tourism server is running on port ${port}`);
-    
+app.listen(port, () => {
+	console.log(`tourism server is running on port ${port}`);
+
 })
