253713418273fce46ecf4bec52dfbc9788d17a5f
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-01-20 10:09:05 +0600

    add verify token

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -7,8 +9,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleWare
-app.use(cors())
+app.use(cors({
+	origin: 'http://localhost:5173',
+	credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3umb5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,7 +27,27 @@ const client = new MongoClient(uri, {
 	}
 });
 
-
+// middleWares
+const logger = (req, res, next) => {
+	console.log('log info',req.method, req.url);
+	next()
+}
+const verifyToken = (req, res, next) => {
+	const token = req?.cookies?.token
+	console.log('token in the middle ware', token);
+	if (!token) {
+		res.status(401).send({message: 'unauthorized access'})
+	}
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+		// err
+		if (err) {
+			res.status(401).send({message: 'unauthorized access'})
+		}
+		req.user = decode
+		next()
+	})
+	
+}
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
@@ -31,7 +57,24 @@ async function run() {
 		const tourismSpotUserCollection = client.db('spotDB').collection('user')
 
 		// jwt token
-		
+		app.post('/jwt', async (req, res) => {
+			const user = req.body
+			console.log('user for token', user);
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3h' })
+			res.cookie('token', token, {
+				httpOnly: true,
+				secure: true,
+				sameSite: 'none'
+			})
+				.send({ success: true })
+		})
+
+		// clear cookie
+		app.post('/logout', (req, res) => {
+			const user = req.body
+			res.clearCookie('token', {maxAge: 0}).send({success: true})
+		})
+		// services related api
 		app.post('/addSpot', async (req, res) => {
 			const addSpot = req.body
 			// console.log(addSpot);
@@ -43,8 +86,13 @@ async function run() {
 			res.send(result)
 		})
 
-		app.get('/addSpot',  async (req, res) => {
-			console.log(req.query.email);
+		app.get('/addSpot', logger, verifyToken,   async (req, res) => {
+			// console.log(req.query.email);
+			console.log('token owner info', req.user);
+			if (req.user.email !== req.query.email) {
+				return res.status(401).send({message: 'unauthorized access2'})
+			}
+			
 			
 			let query = {}
 			if(req.query?.email){
@@ -55,7 +103,7 @@ async function run() {
 			res.send(result)
 		})
 
-		app.get('/addSpot/:id', verifyToken, async (req, res) => {
+		app.get('/addSpot/:id', async (req, res) => {
 			const id = req.params.id
 			const query = { _id: new ObjectId(id) }
 			const result = await tourismSpotCollection.findOne(query)
