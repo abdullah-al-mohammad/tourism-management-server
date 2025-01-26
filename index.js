e202e025d85c21e6cd8342de9e8680de49c341c1
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-01-26 23:57:58 +0600

    set pagination by  skip and limit set addspot id

--- index.js
+++ index.js
@@ -36,22 +36,29 @@ const verifyToken = (req, res, next) => {
 	const token = req?.cookies?.token
 	console.log('token in the middle ware', token);
 	if (!token) {
-		res.status(401).send({message: 'unauthorized access'})
+		return res.status(401).send({message: 'unauthorized access'})
 	}
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+        console.log('decode',decode);
+        
 		// err
 		if (err) {
-			res.status(401).send({message: 'unauthorized access'})
+			return res.status(401).send({message: 'unauthorized access'})
 		}
 		req.user = decode
 		next()
 	})
 	
 }
+const cookieOption ={
+				httpOnly: true,
+				secure: true,
+				sameSite: 'none'
+			}
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
 
 		const tourismSpotCollection = client.db('spotDB').collection('addSpot')
 		const tourismSpotUserCollection = client.db('spotDB').collection('user')
@@ -60,19 +67,15 @@ async function run() {
 		app.post('/jwt', async (req, res) => {
 			const user = req.body
 			console.log('user for token', user);
-			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3h' })
-			res.cookie('token', token, {
-				httpOnly: true,
-				secure: true,
-				sameSite: 'none'
-			})
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+			res.cookie('token', token, cookieOption)
 				.send({ success: true })
 		})
 
 		// clear cookie
 		app.post('/logout', (req, res) => {
 			const user = req.body
-			res.clearCookie('token', {maxAge: 0}).send({success: true})
+			res.clearCookie('token', {...cookieOption, maxAge: 0}).send({success: true})
 		})
 		// services related api
 		app.post('/addSpot', async (req, res) => {
@@ -81,24 +84,27 @@ async function run() {
 			
 			const {creator} = addSpot
 			addSpot.creator = new ObjectId(creator)
-			console.log('add tourist spot', addSpot);
+			// console.log('add tourist spot', addSpot);
 			const result = await tourismSpotCollection.insertOne(addSpot)
 			res.send(result)
 		})
 
-		app.get('/addSpot', logger, verifyToken,   async (req, res) => {
+		app.get('/addSpot', logger,  async (req, res) => {
 			// console.log(req.query.email);
-			console.log('token owner info', req.user);
-			if (req.user.email !== req.query.email) {
-				return res.status(401).send({message: 'unauthorized access2'})
-			}
+            console.log('token owner info', req.user);
+            // const user = req.user
+			// if (req.user.email !== req.query.email) {
+			// 	return res.status(401).send({message: 'unauthorized access2'})
+			// }
+			const page = parseInt(req.query.page)
+			const size = parseInt(req.query.size)
 			
 			
 			let query = {}
 			if(req.query?.email){
 				query={email: req.query.email}
 			}
-			const cursor = tourismSpotCollection.find(query)
+			const cursor = tourismSpotCollection.find(query).skip(page*size).limit(size)
 			const result = await cursor.toArray()
 			res.send(result)
 		})
@@ -110,22 +116,18 @@ async function run() {
 			res.send(result)
 		})
 
-		// app.get('/addSpot', async (req, res) => {
-		// 	console.log(req.query);
-			
-		// 	const userId = req.query.creator
-		// 	// console.log(userId);
-			
-		// 	const result = await tourismSpotCollection.find().toArray()
-		// 	res.send(result)
-		// })
+		// pagination
+		app.get('/dataCount', async(req, res) => {
+			const count = await tourismSpotCollection.estimatedDocumentCount()
+			res.send({count})
+		})
 
 
 		// user Information
 		app.post("/user", async (req, res) => {
 			const users = req.body
 			const result = await tourismSpotUserCollection.insertOne(users)
-			console.log(result);
+			// console.log(result);
 			res.send(result)
 		})
 
@@ -139,12 +141,12 @@ async function run() {
 				if (result) {
 					res.send(result)
 				} else {
-					res.status(404).send({ error: 'User not found' })
+					return res.status(404).send({ error: 'User not found' })
 				}
 
 			} catch (err) {
-				console.error('Error fetching user:', err);
-				res.status(500).send({ error: 'Internal Server Error' });
+				// console.error('Error fetching user:', err);
+				return res.status(500).send({ error: 'Internal Server Error' });
 			}
 		})
 
@@ -157,8 +159,8 @@ async function run() {
 
 
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
-		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		// await client.db("admin").command({ ping: 1 });
+		// console.log("Pinged your deployment. You successfully connected to MongoDB!");
 	} finally {
 		// Ensures that the client will close when you finish/error
 		// await client.close();
